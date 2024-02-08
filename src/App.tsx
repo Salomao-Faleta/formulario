92d0f61f4afd456eb121b1c5b6d81f2151881762
Salomao-Faleta <faletas41@gmail.com>
2024-02-07 22:21:39 -0300

    fazendo a estilização
    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Form } from "./componetes/Form";
 
 function App() {
     return (
-        <div>
-            <h1 className="text-3xl text-red-700">Inscreva-se</h1>
-            <p>Assine nossa plataforma e mantenha-se informado</p>
-            <div>
+        <div className="bg-slate-800 min-h-screen w-full flex flex-col items-center justify-center">
+            <h1 className="font-bold text-white text-4xl">Inscreva-se</h1>
+            <p className="text-white">Assine nossa plataforma e mantenha-se informado</p>
+            <div className="w-96 mt-4 bg-stone-200 px-4 py-5 rounded-md">
                 <Form/>
             </div>
-            <p>Ao se inscrever, você passará a receber nossos e-mails com as melhores dicas, novidades e ofertas</p>
+            <p className="text-slate-100 text-xs w-96 mt-2 text-center">Ao se inscrever, você passará 
+                a receber nossos e-mails com as melhores dicas, novidades e ofertas.
+            </p>
         </div>
     )
 }
--- src/componetes/Form.tsx
+++ src/componetes/Form.tsx
@@ -1,22 +1,28 @@
 export function Form() {
     return (
-        <form>
-            <div>
-                <label htmlFor="nome">Nome</label>
-                <input type="text" id="nome" placeholder="Digite seu nome"/>
+        <form className="flex flex-col gap-3">
+            <div className="flex flex-col">
+                <label className="text-sm" htmlFor="nome">Nome</label>
+                <input type="text" id="nome" placeholder="Digite seu nome" className="rounded-md py-2 px-2 text-sm"/>
             </div>
-            <div>
-                <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" placeholder="Digite seu melhor e-mail"/>
+
+            <div className="flex flex-col">
+                <label className="text-sm" htmlFor="email">E-mail</label>
+                <input type="email" id="email" placeholder="Digite seu melhor e-mail" className="rounded-md py-2 px-2 text-sm"/>
             </div>
-            <div>
-                <a href="#">Leia os termos</a>
+
+            <div className="flex flex-col">
                 <div>
-                    <input type="checkbox"/>
-                    <label>Concordo com os termos</label>
+                    <span>➜</span>
+                    <a href="#" className="text-sx underline mb-2">Leia os termos</a>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <input type="checkbox" />
+                    <label htmlFor="agreen">Concordo com os termos</label>
                 </div>
             </div>
-            <button>Cadastre-se</button>
+            <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 text-white rounded-md">Cadastre-se</button>
         </form>
 
     );
